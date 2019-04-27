755afef8534f5988f69e58c8f6160cfd09e27994
momo <callmedevmomo@gmail.com>
2019-04-27 15:02:10 +0900

    Stateless Functional Components

--- src/App.js
+++ src/App.js
@@ -2,40 +2,60 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-const movies = [
-  {
-    title: "나의특별한형제",
-    poster: "http://www.theviewers.co.kr/news/photo/201903/30002_25680_2043.jpg"
-  },
-  {
-    title: "Avengers",
-    poster: "http://i.ebayimg.com/images/i/111802852108-0-1/s-l1000.jpg"
-  },
-  {
-    title: "Little forest",
-    poster:
-      "https://media-cache.cinematerial.com/p/500x/lkbtp09s/little-forest-south-korean-movie-poster.jpg"
-  },
-  {
-    title: "Call Me By Your Name",
-    poster:
-      "https://media-cache.cinematerial.com/p/500x/fsvwpcn1/call-me-by-your-name-slovak-movie-poster.jpg"
-  },
-  {
-    title: "Final Portrait",
-    poster:
-      "https://media-cache.cinematerial.com/p/500x/iymbu1i6/final-portrait-spanish-movie-poster.jpg"
-  }
-];
 class App extends Component {
+  // Render : componentWillMount() -> render() -> componentDidMount()
+  // Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+  // State 변경(.setState({})) --> Render 자동 활성화
+  state = {};
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: "나의특별한형제",
+            poster:
+              "http://www.theviewers.co.kr/news/photo/201903/30002_25680_2043.jpg"
+          },
+          {
+            title: "Avengers",
+            poster: "http://i.ebayimg.com/images/i/111802852108-0-1/s-l1000.jpg"
+          },
+          {
+            title: "Little forest",
+            poster:
+              "https://media-cache.cinematerial.com/p/500x/lkbtp09s/little-forest-south-korean-movie-poster.jpg"
+          },
+          {
+            title: "Call Me By Your Name",
+            poster:
+              "https://media-cache.cinematerial.com/p/500x/fsvwpcn1/call-me-by-your-name-slovak-movie-poster.jpg"
+          },
+          {
+            title: "Final Portrait",
+            poster:
+              "https://media-cache.cinematerial.com/p/500x/iymbu1i6/final-portrait-spanish-movie-poster.jpg"
+          },
+          {
+            title: "Leon",
+            poster:
+              "https://i.pinimg.com/736x/c9/d5/12/c9d5127cb21f42f462dac07d6b4189fa--jean-reno-figurative.jpg"
+          }
+        ]
+      });
+    }, 5000);
+  }
+
+  _renderMovies = () => {
+    const movie = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    });
+    return movie;
+  };
   render() {
     return (
+      // function ()
       <div className="App">
-        {movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          );
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading..."}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,29 +1,54 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-class Movie extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-  };
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+// ******Smart Component
+
+// class Movie extends Component {
+//   static propTypes = {
+//     title: PropTypes.string.isRequired,
+//     poster: PropTypes.string.isRequired
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <MoviePoster poster={this.props.poster} />
+//         <h1>{this.props.title}</h1>
+//       </div>
+//     );
+//   }
+// }
+// class MoviePoster extends Component {
+//   static propTypes = {
+//     poster: PropTypes.string.isRequired
+//   };
+//   render() {
+//     return <img src={this.props.poster} alt="Movie Poster" />;
+//   }
+// }
+
+// dumb component(stateless functional) : 한 개의 props의 존재만으로도 가능 :: Component Life Cycle , render() (x)
+// dumb component => class가 아니기 때문에 this.props 사용 (x) *변경이 없는 Component에서 사용
+//{title, poster} are the props passed from App.js to the Movie Component.
+
+function Movie({ title, poster }) {
+  return (
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
-class MoviePoster extends Component {
-  static propTypes = {
-    poster: PropTypes.string.isRequired
-  };
-  render() {
-    return <img src={this.props.poster} />;
-  }
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
+
+function MoviePoster({ poster }) {
+  return <img src={poster} alt="Movie Poster" />;
 }
+MoviePoster.propTypes = { poster: PropTypes.string.isRequired };
+
 export default Movie;
