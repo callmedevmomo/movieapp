3fdd9c495ad0be4d724580b6cbf1eb22c4f5b906
momo <callmedevmomo@gmail.com>
2019-04-27 18:04:40 +0900

    Updates

---
+++ precache-manifest.87f2ede2d24ebe0c1e87a1db4066e9a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28ca1c1c3473cd2e742fdfd7de3a4e2",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "b1acd5b05bfc99b51e43",
+    "url": "/movieapp/static/css/main.a9ffb67c.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movieapp/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "b1acd5b05bfc99b51e43",
+    "url": "/movieapp/static/js/main.8eac2362.chunk.js"
+  },
+  {
+    "revision": "4e19d316896026f2633d",
+    "url": "/movieapp/static/js/runtime~main.839b6e54.js"
+  }
+]);
