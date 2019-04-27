3004f24288a3971d806a41e0bcd3c3b37321792e
momo <callmedevmomo@gmail.com>
2019-04-27 18:00:14 +0900

    is done...

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import Movie from "./Movie";
 
 class App extends Component {
-  // Render : componentWillMount() -> render() -> componentDidMount()
-  // Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
-  // State 변경(.setState({})) --> Render 자동 활성화
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
   state = {};
+
   componentDidMount() {
-    this._getMoives();
+    this._getMovies();
   }
 
   _renderMovies = () => {
@@ -16,7 +17,7 @@ class App extends Component {
       return (
         <Movie
           title={movie.title_english}
-          poster={movie.medium_cover_image}
+          poster={movie.large_cover_image}
           key={movie.id}
           genres={movie.genres}
           synopsis={movie.synopsis}
@@ -26,8 +27,7 @@ class App extends Component {
     return movies;
   };
 
-  _getMoives = async () => {
-    // .then (.then(.then....~~)) ==> CALLBACK HELL!!! ==> async await
+  _getMovies = async () => {
     const movies = await this._callApi();
     this.setState({
       movies
@@ -35,21 +35,19 @@ class App extends Component {
   };
 
   _callApi = () => {
-    return (
-      fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
-        // modern JS &&  (Arrow Function...)
-        //ReadableStream (01010101..) => json()
-        .then(beautiful => beautiful.json())
-        .then(json => json.data.movies)
-        .catch(err => console.log(err))
-    );
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+    )
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
   };
 
   render() {
+    const { movies } = this.state;
     return (
-      // function ()
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : "Loading..."}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,54 +1,41 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import PropTypes from "prop-types";
+import LinesEllipsis from "react-lines-ellipsis";
 import "./Movie.css";
-
-// ******Smart Component
-
-// class Movie extends Component {
-//   static propTypes = {
-//     title: PropTypes.string.isRequired,
-//     poster: PropTypes.string.isRequired
-//   };
-//   render() {
-//     return (
-//       <div>
-//         <MoviePoster poster={this.props.poster} />
-//         <h1>{this.props.title}</h1>
-//       </div>
-//     );
-//   }
-// }
-// class MoviePoster extends Component {
-//   static propTypes = {
-//     poster: PropTypes.string.isRequired
-//   };
-//   render() {
-//     return <img src={this.props.poster} alt="Movie Poster" />;
-//   }
-// }
-
-// dumb component(stateless functional) : 한 개의 props의 존재만으로도 가능 :: Component Life Cycle , render() (x)
-// dumb component => class가 아니기 때문에 this.props 사용 (x) *변경이 없는 Component에서 사용
-//{title, poster} are the props passed from App.js to the Movie Component.
 
 function Movie({ title, poster, genres, synopsis }) {
   return (
     <div className="Movie">
-      <div className="Movie_Columns">
+      <div className="Movie__Column">
         <MoviePoster poster={poster} alt={title} />
       </div>
-      <div className="Movie_Columns">
+      <div className="Movie__Column">
         <h1>{title}</h1>
-        <div className="Movie_Genres">
+        <div className="Movie__Genres">
           {genres.map((genre, index) => (
             <MovieGenre genre={genre} key={index} />
           ))}
         </div>
-        <p className="Movie_Synopsis">{synopsis}</p>
+        <div className="Movie__Synopsis">
+          <LinesEllipsis
+            text={synopsis}
+            maxLine="3"
+            ellipsis="..."
+            trimRight
+            basedOn="letters"
+          />
+        </div>
       </div>
     </div>
   );
+}
+
+function MoviePoster({ poster, alt }) {
+  return <img src={poster} alt={alt} title={alt} className="Movie__Poster" />;
+}
+
+function MovieGenre({ genre }) {
+  return <span className="Movie__Genre">{genre}</span>;
 }
 
 Movie.propTypes = {
@@ -57,20 +44,14 @@ Movie.propTypes = {
   genres: PropTypes.array.isRequired,
   synopsis: PropTypes.string.isRequired
 };
-//({poster}) is taking the 'poster variable from the props.
-// function MoviePoster(props) -> props.poster
-// or
-// function MoviePoster({poster}) -> poster
-function MoviePoster({ poster, alt }) {
-  return <img src={poster} alt={alt} title={alt} className="Movie_Poster" />;
-}
-function MovieGenre({ genre }) {
-  console.log(genre);
-  return <span className="Movie_Genre">{genre}</span>;
-}
+
 MoviePoster.propTypes = {
   poster: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired
 };
-MovieGenre.propTypes = { genre: PropTypes.string.isRequired };
+
+MovieGenre.propTypes = {
+  genre: PropTypes.string.isRequired
+};
+
 export default Movie;
