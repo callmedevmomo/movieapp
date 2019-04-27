e31f88d5acd08906d6f4172d322a1ca0d64b2eba
momo <callmedevmomo@gmail.com>
2019-04-27 15:10:32 +0900

    AJAX in React

--- src/App.js
+++ src/App.js
@@ -8,41 +8,7 @@ class App extends Component {
   // State 변경(.setState({})) --> Render 자동 활성화
   state = {};
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "나의특별한형제",
-            poster:
-              "http://www.theviewers.co.kr/news/photo/201903/30002_25680_2043.jpg"
-          },
-          {
-            title: "Avengers",
-            poster: "http://i.ebayimg.com/images/i/111802852108-0-1/s-l1000.jpg"
-          },
-          {
-            title: "Little forest",
-            poster:
-              "https://media-cache.cinematerial.com/p/500x/lkbtp09s/little-forest-south-korean-movie-poster.jpg"
-          },
-          {
-            title: "Call Me By Your Name",
-            poster:
-              "https://media-cache.cinematerial.com/p/500x/fsvwpcn1/call-me-by-your-name-slovak-movie-poster.jpg"
-          },
-          {
-            title: "Final Portrait",
-            poster:
-              "https://media-cache.cinematerial.com/p/500x/iymbu1i6/final-portrait-spanish-movie-poster.jpg"
-          },
-          {
-            title: "Leon",
-            poster:
-              "https://i.pinimg.com/736x/c9/d5/12/c9d5127cb21f42f462dac07d6b4189fa--jean-reno-figurative.jpg"
-          }
-        ]
-      });
-    }, 5000);
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating");
   }
 
   _renderMovies = () => {
